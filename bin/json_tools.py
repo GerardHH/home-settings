999b3cc23b361a5e82235508c78a412889ad9691
Gerard Heshusius <gerard.heshusius@ravenind.com>
2020-09-28 11:47:24 +0200

    Raven: bin/json_tools.py: Add __is_valid_query

--- bin/json_tools.py
+++ bin/json_tools.py
@@ -9,7 +9,6 @@ import json
 import os.path
 
 # TODO: Add check if json is installed and report error if it isn't
-# TODO: Add is_valid_query for --query
 # TODO: Multiple objects for --add? Now it results in a cryptic error
 # TODO: Add search on attribute in JSON list for --query
 # TODO: Add check if file is readable JSON
@@ -50,6 +49,11 @@ def __is_valid_json(string):
     return json.loads(string)
 
 
+def __is_valid_query(string):
+    ''' Check if string is a valid point saperated query and return it. '''
+    return string.split('.')
+
+
 def __print_object(object, verbose):
     '''Print JSON or value.'''
     __verbose_print('Printing object', verbose)
@@ -80,6 +84,7 @@ def __verbose_print(printable, verbose, heading=True):
 def json_tools(file, add=None, backup=False, print_result=False, query=None, verbose=False, write=False):
     ''' Execute JSON tools dependant on enabled features.
     add - should be dict/list (JSON)
+    query - should be list of strings
     '''
     if backup:
         __backup(path=file, verbose=verbose)
@@ -87,7 +92,7 @@ def json_tools(file, add=None, backup=False, print_result=False, query=None, ver
     with open(file, 'r') as json_file:
         __verbose_print('Loading file to object', verbose)
         root = json.load(json_file)
-    object = root if not query else __query(root=root, query_list=query.split('.'), verbose=verbose)
+    object = root if not query else __query(root=root, query_list=query, verbose=verbose)
     if add:
         __add(object=object, addition=add, verbose=verbose)
     if print_result:
@@ -106,7 +111,7 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--add', type=__is_valid_json, help='Add JSON object(s) to existing object/list in the form of `{"key":"value}`. Can be used to overwrite attributes by adding a key with the same name.')
     parser.add_argument('-b', '--backup', action='store_true', help='Write a backup of file by appending .backup. If a backup already exist, then this step is skipped.')
     parser.add_argument('-p', '--print_result', action='store_true', help='Print JSON file to stdout. In case of --query, will print the query result.')
-    parser.add_argument('-q', '--query', help='Query an object in the form of `key.index.key` starting from root, Default = root.')
+    parser.add_argument('-q', '--query', type=__is_valid_query, help='Query an object in the form of `key.index.key` starting from root, Default = root.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print intermediate steps.')
     parser.add_argument('-w', '--write', action='store_true', help='Write JSON in place.')
     args = parser.parse_args()
