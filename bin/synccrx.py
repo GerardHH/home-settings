013628d072e8350d323bd20ba4ec2e7a986c0ff3
Gerard Heshusius <gerard.heshusius@ravenind.com>
2020-11-02 13:32:30 +0100

    Raven: bin/synccrx.py: Simplify script and use submodule instead of os.system

--- bin/synccrx.py
+++ bin/synccrx.py
@@ -1,37 +1,43 @@
 #! /usr/bin/python3
 
-from os import chdir, system
+from os import chdir
+from subprocess import run
 import argparse
 import env
 
-chdir(env.CRX)
 
-parser = argparse.ArgumentParser(description='Sync the whole project')
-parser.add_argument('-n', '--no-sync', action='store_true', help='Skip syncing all the repo\'s')
-parser.add_argument('-d', '--dry-run', action='store_true', help='Only show the command that will be executed')
-parser.set_defaults(branch=None, tag=None, reset=None) # Hack to be able to check them
-subparsers = parser.add_subparsers(title='Modes', description='Used to checkout either a branch or tag')
+def __checkout(branch: str = 'master', tag: str = None, abort_on_errors: bool = False, pager: bool = False):
+    ''' Checkout branch/commit in every repo in CRx. Fast forward merge to ${git remote}/branch if applicable'''
+    git_command = 'git checkout {commit}{merge_command}'.format(
+        commit=branch if not tag else tag,
+        merge_command=' && git merge $(git remote)/{}'.format(branch) if not tag else '')
+    run([env.REPO, 'forall', '{}'.format('--abort-on-errors' if abort_on_errors else ''), '{}'.format('-pv' if pager else ''), '--command', '{}'.format(git_command)], check=True)
 
-parser_branch = subparsers.add_parser('branch', help='Checkout a branch after syncing')
-parser_branch.add_argument('-b', '--branch', default='master', help='The branch to checkout, default: master')
-parser_branch.add_argument('-r', '--reset', action='store_true', help='Execute git reset --hard cpro-gitlab/$branch')
 
-parser_tag = subparsers.add_parser('tag', help='Checkout a tag after syncing')
-parser_tag.add_argument('tag', help='The tag to checkout')
+def __sync():
+    ''' Download sources and submodules from server. '''
+    run([env.REPO, 'sync', '--prune', '--fetch-submodules'], check=True)
 
-args = parser.parse_args()
 
-if not args.branch and not args.tag:
-    parser.error('Either branch or tag mode are required')
+def __update_submodules(abort_on_errors: bool = False):
+    ''' Update all submodules in CRx. '''
+    run([env.REPO, 'forall', '{}'.format('--abort-on-errors' if abort_on_errors else ''), '--command', 'git submodule update --init --recursive'], check=True)
 
-command = '{sync}{repo} forall -c "git checkout {commit}{reset}"'.format(
-    sync='{repo} sync --fetch-submodules && '.format(repo=env.REPO) if not args.no_sync else '',
-    repo=env.REPO,
-    commit=args.branch if args.branch else args.tag,
-    reset=' && git reset --hard cpro-gitlab/{}'.format(args.branch) if args.reset else ''
-    )
 
-if args.dry_run:
-    print(command)
-else:
-    system(command)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Sync the whole CRx project')
+    parser.add_argument('-a', '--abort-on-errors', action='store_true', help='Exit subcommands when an error is encountered')
+    parser.add_argument('-s', '--skip-sync', action='store_true', help='Skip the download from server step and only execute the checkout (and potential merge)')
+    parser.add_argument('-p', '--pager', action='store_true', help='Show pager with per repo verbose output for checkout step')
+    exclusive_group = parser.add_mutually_exclusive_group()
+    exclusive_group.add_argument('-b', '--branch', default='master', help='The branch to checkout after the sync, default: master')
+    exclusive_group.add_argument('-t', '--tag', help='The tag to checkout after the sync')
+
+    args = parser.parse_args()
+
+    chdir(env.CRX)
+
+    if not args.skip_sync:
+        __sync()
+    __checkout(branch=args.branch, tag=args.tag, abort_on_errors=args.abort_on_errors, pager=args.pager)
+    __update_submodules(abort_on_errors=args.abort_on_errors)
