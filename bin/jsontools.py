a6e0095aca8ebb01ac7a635d58a02e0c593c2908
Gerard Heshusius <gerard.heshusius@ravenind.com>
2020-11-19 11:26:16 +0100

    bin/jsontools.py: __find: Change to generator to support multiple results
    Also changed the unions to lower capitals

--- bin/jsontools.py
+++ bin/jsontools.py
@@ -17,11 +17,11 @@ import os.path
 # TODO: Add --find_key and change --find to --find_attribute
 
 
-__JSON_OBJECTS = Union[Dict, List]
-__JSON_TYPES = Union[bool, Dict, float, int, List, str]
+__json_object = Union[Dict, List]
+__json_type = Union[bool, Dict, float, int, List, str]
 
 
-def __add(object: __JSON_OBJECTS, addition: __JSON_OBJECTS, verbose: bool) -> None:
+def __add(object: __json_object, addition: __json_object, verbose: bool) -> None:
     '''Add addition JSON object to abject.'''
     __verbose_print('Adding to object', verbose)
     if isinstance(object, dict):
@@ -43,35 +43,22 @@ def __backup(path: str, verbose: bool) -> None:
         __verbose_print('Backup already exists, ignoring...', verbose, False)
 
 
-def __find(root: __JSON_TYPES, attr_value: str, verbose: bool) -> Tuple[bool, __JSON_TYPES]:
-    ''' Find an attribute and return its parent. '''
-    __verbose_print('Finding attribute value: {}'.format(attr_value), verbose)
-    __verbose_print('Current value: {}'.format(root), verbose, False)
+def __find(object: __json_object, attr_value: str, verbose: bool = False) -> Dict:
+    ''' Find attr_value and yield the Dict that contains it.'''
+    if verbose:
+        print('-'*10)
+        print('Searching for attribute: "{}" in {}'.format(attr_value, object))
+    if isinstance(object, dict):
+        for key, value in object.items():
+            if isinstance(value, dict) or isinstance(value, list):
+                yield from __find(value, attr_value, verbose)
+            elif str(value).lower() == attr_value.lower():
+                yield object
 
-    if isinstance(root, bool):
-        return (str(root).lower == attr_value.lower(), root)
-
-    elif isinstance(root, dict):
-        for value in root.values():
-            __verbose_print('Searching in: {}'.format(value), verbose, False)
-            found, object = __find(root=value, attr_value=attr_value, verbose=verbose)
-            if found:
-                return (found, object if isinstance(object, dict) or isinstance(object, list) else root)
-        return (False, root)
-
-    elif isinstance(root, float) or isinstance(root, int):
-        return (str(root) == attr_value, root)
-
-    elif isinstance(root, list):
-        for value in root:
-            __verbose_print('Searching in: {}'.format(value), verbose, False)
-            found, object = __find(root=value, attr_value=attr_value, verbose=verbose)
-            if found:
-                return (found, object)
-        return (False, root)
-
-    elif isinstance(root, str):
-        return (root == attr_value, root)
+    elif isinstance(object, list):
+        for element in object:
+            if isinstance(element, dict) or isinstance(element, list):
+                yield from __find(element, attr_value, verbose)
 
 
 def __is_valid_file(path: str) -> str:
@@ -81,7 +68,7 @@ def __is_valid_file(path: str) -> str:
     raise argparse.ArgumentTypeError(path + ' is not a valid path')
 
 
-def __is_valid_json(string: str) -> __JSON_OBJECTS:
+def __is_valid_json(string: str) -> __json_object:
     ''' Check if string is a valid JSON. '''
     return json.loads(string)
 
@@ -91,14 +78,14 @@ def __is_valid_query(string: str) -> List[str]:
     return string.split('.')
 
 
-def __print_object(object: __JSON_TYPES, verbose: bool) -> None:
+def __print_object(object: __json_type, verbose: bool) -> None:
     '''Print JSON or value.'''
     __verbose_print('Printing object', verbose)
     json.dump(object, stdout, indent=4)
     print('') # For some reason following prints will mess with the json.dump but printing a new line seems to fix it.
 
 
-def __query(root: __JSON_OBJECTS, query_list: List[str], verbose: bool) -> __JSON_TYPES:
+def __query(root: __json_object, query_list: List[str], verbose: bool) -> __json_type:
     '''Query object based on query_list.'''
     __verbose_print('Querying object', verbose)
     object = root
@@ -118,8 +105,8 @@ def __verbose_print(printable, verbose: bool, heading: bool = True) -> None:
             print(printable, file=stderr)
 
 
-def json_tools(file: str, add: __JSON_OBJECTS = None, backup: bool = False, find: str = None, print_result: bool = False, query: List[str] = None, verbose: bool = False, write: bool = False) -> None:
-    ''' Execute JSON tools dependant on enabled features. '''
+def json_tools(file: str, add: __json_object = None, backup: bool = False, find: str = None, find_all: str = None, print_result: bool = False, query: List[str] = None, verbose: bool = False, write: bool = False) -> None:
+    ''' Execute JSON tools dependant on enabled features. If find_all is provided, find and add will be ignored.'''
     if backup:
         __backup(path=file, verbose=verbose)
     root = {}
@@ -127,10 +114,19 @@ def json_tools(file: str, add: __JSON_OBJECTS = None, backup: bool = False, find
         __verbose_print('Loading file to object', verbose)
         root = json.load(json_file)
     object = root if not query else __query(root=root, query_list=query, verbose=verbose)
-    found, object = (False, object) if not find else __find(root=object, attr_value=find, verbose=verbose)
-    if find and not found:
-        raise NameError('Could not find {} in the JSON document'.format(find))
-    if add:
+    if find and not find_all:
+        try:
+            object = next(__find(object=object, attr_value=find, verbose=verbose))
+        except StopIteration:
+            raise NameError('Could not find "{}" in the JSON document'.format(find))
+    elif not find and find_all:
+        result = []
+        for found_object in __find(object=object, attr_value=find_all, verbose=verbose):
+            result.append(found_object)
+        if not result:
+            raise NameError('Could not find "{}" in the JSON document'.format(find_all))
+        object = result
+    if add and not find_all:
         __add(object=object, addition=add, verbose=verbose)
     if print_result:
         __print_object(object=object, verbose=verbose)
@@ -152,5 +148,6 @@ if __name__ == '__main__':
     parser.add_argument('-q', '--query', type=__is_valid_query, help='Query an object in the form of `key.index.key` starting from root, Default = root.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print intermediate steps.')
     parser.add_argument('-w', '--write', action='store_true', help='Write JSON in place.')
+    parser.add_argument('--find-all', help='Find all JSON objects that contain the supplied attribute value.')
     args = parser.parse_args()
     json_tools(**args.__dict__)
